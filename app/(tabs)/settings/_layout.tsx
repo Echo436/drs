c904b3b35cba39ff2a5a5fbc66fe0bebda5687f0
Echo <819905193@qq.com>
2025-04-04 22:46:22 +0800

    refactor(settings): 使用 ThemedView 替换 View 组件
    为了保持主题一致性，将 Settings 页面中的 View 组件替换为 ThemedView 组件

--- app/(tabs)/settings/_layout.tsx
+++ app/(tabs)/settings/_layout.tsx
@@ -7,12 +7,13 @@ import { setPrimaryColor } from '@/storage/themeSlice';
 import { getLoadedFonts } from "expo-font";
 
 import { getTeamsColor } from "@/constants/Colors";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function Settings() {
   const primaryColor = useAppSelector(state => state.theme.primaryColor);
   const dispatch = useAppDispatch();
   return (
-    <View style={layoutStyles.centerContainer}>
+    <ThemedView style={layoutStyles.centerContainer}>
       <Button title="法拉利" onPress={() => {
         dispatch(setPrimaryColor(getTeamsColor('ferrari')));
       }} />
@@ -25,6 +26,6 @@ export default function Settings() {
       <Button title="梅赛德斯" onPress={() => {
         dispatch(setPrimaryColor(getTeamsColor('mercedes')));
       }} />
-    </View>
+    </ThemedView>
   );
 }
