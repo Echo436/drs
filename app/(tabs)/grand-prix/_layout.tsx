4e84c771a78587e3d6fd1aba904d1c6c83cc1e12
Echo <819905193@qq.com>
2025-04-04 22:45:52 +0800

    refactor(grand-prix): 重构大奖赛布局组件，替换FirstPage为GrandPrixDetail
    将FirstPage从Current组件替换为GrandPrixDetail组件，并传递isCurrentPage参数。此更改旨在优化页面结构和功能，提升用户体验。

--- app/(tabs)/grand-prix/_layout.tsx
+++ app/(tabs)/grand-prix/_layout.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { TabPageLayout } from '@/components/TabPageLayout';
 import { t } from '@/i18n/utils';
-import Current from './current';
 import Season from './season';
+import Settings from '../settings/_layout';
+import GrandPrixDetail from '@/app/race/[raceId]';
 
 // 大奖赛布局组件，使用通用TabPageLayout组件实现标签页切换功能
 export default function GrandPrixLayout() {
     return (
         <TabPageLayout
-            FirstPage={Current}
+            FirstPage={() => <GrandPrixDetail isCurrentPage={true} />}
+            // FirstPage={Settings}
             SecondPage={Season}
             firstTabLabel={t('Current', 'tabs')}
             secondTabLabel={t('Season', 'tabs')}
