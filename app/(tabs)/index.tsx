dff9112703676de8780a515168a458237d968ee9
Echo <819905193@qq.com>
2025-04-04 22:45:28 +0800

    fix: 修正首页重定向路径
    将首页重定向路径从 `/grand-prix/current` 更改为 `/grand-prix`，以匹配最新的路由配置

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Redirect } from "expo-router";
 
 const Index = () => {
-	return <Redirect href="/grand-prix/current" />;
+	return <Redirect href="/grand-prix" />;
 };
 export default Index;
