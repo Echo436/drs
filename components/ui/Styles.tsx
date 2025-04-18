6de96b1448f597b0437e31b8566e3e25ad25045d
Echo <819905193@qq.com>
2025-04-19 01:36:15 +0800

    style: 移除centerContainer中的justifyContent属性
    为了简化布局样式并减少不必要的属性，移除了centerContainer中的justifyContent属性，保持布局更加简洁。

--- components/ui/Styles.tsx
+++ components/ui/Styles.tsx
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 export const layoutStyles = StyleSheet.create({
     centerContainer: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
     },
     listContainer: {
