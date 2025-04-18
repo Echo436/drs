81dbe02899aac623b87339f37c3cf452fce25bdd
Echo <819905193@qq.com>
2025-04-19 01:36:39 +0800

    feat(电路图片): 添加详细赛道图片映射及获取函数
    添加了 `circuitDetailMap` 映射，用于存储详细赛道图片路径，并新增 `getCircuitDetailImage` 函数以根据赛道 ID 获取详细图片。这些更改是为了支持应用中显示更详细的赛道图像。

--- constants/CircuitImages.ts
+++ constants/CircuitImages.ts
@@ -29,6 +29,33 @@ const circuitImageMap: { [key: string]: ImageSourcePropType } = {
     zandvoort: require('@/assets/images/circuit-simple/zandvoort.png'),
 };
 
+const circuitDetailMap: { [key: string]: ImageSourcePropType } = {
+    albert_park: require('@/assets/images/circuit-detail/albert_park.png'),
+    americas: require('@/assets/images/circuit-detail/americas.png'),
+    bahrain: require('@/assets/images/circuit-detail/bahrain.png'),
+    baku: require('@/assets/images/circuit-detail/baku.png'),
+    catalunya: require('@/assets/images/circuit-detail/catalunya.png'),
+    hungaroring: require('@/assets/images/circuit-detail/hungaroring.png'),
+    imola: require('@/assets/images/circuit-detail/imola.png'),
+    interlagos: require('@/assets/images/circuit-detail/interlagos.png'),
+    jeddah: require('@/assets/images/circuit-detail/jeddah.png'),
+    losail: require('@/assets/images/circuit-detail/losail.png'),
+    marina_bay: require('@/assets/images/circuit-detail/marina_bay.png'),
+    miami: require('@/assets/images/circuit-detail/miami.png'),
+    monaco: require('@/assets/images/circuit-detail/monaco.png'),
+    monza: require('@/assets/images/circuit-detail/monza.png'),
+    red_bull_ring: require('@/assets/images/circuit-detail/red_bull_ring.png'),
+    rodriguez: require('@/assets/images/circuit-detail/rodriguez.png'),
+    shanghai: require('@/assets/images/circuit-detail/shanghai.png'),
+    silverstone: require('@/assets/images/circuit-detail/silverstone.png'),
+    spa: require('@/assets/images/circuit-detail/spa.png'),
+    suzuka: require('@/assets/images/circuit-detail/suzuka.png'),
+    vegas: require('@/assets/images/circuit-detail/vegas.png'),
+    villeneuve: require('@/assets/images/circuit-detail/villeneuve.png'),
+    yas_marina: require('@/assets/images/circuit-detail/yas_marina.png'),
+    zandvoort: require('@/assets/images/circuit-detail/zandvoort.png'),
+}
+
 // Function to get the image source for a given circuitId.
 // Returns undefined if the circuitId is not found.
 export const getCircuitImage = (circuitId: string | undefined): ImageSourcePropType | undefined => {
@@ -38,3 +65,11 @@ export const getCircuitImage = (circuitId: string | undefined): ImageSourcePropT
     // Return undefined if the circuitId is not found
     return undefined;
 };
+
+export const getCircuitDetailImage = (circuitId: string | undefined): ImageSourcePropType | undefined => {
+    if (circuitId && circuitDetailMap[circuitId]) {
+        return circuitDetailMap[circuitId];
+    }
+    // Return undefined if the circuitId is not found
+    return undefined;
+}
