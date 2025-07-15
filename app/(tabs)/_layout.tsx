935440a9c756a1efbba74ab1b6c776e3bd4c84d2
Echo <819905193@qq.com>
2025-07-16 01:26:04 +0800

    feat: 添加React Native Bottom Tabs相关依赖

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,46 +1,44 @@
-import { Tabs } from 'expo-router';
+import { Tabs } from '@/components/bottom-tabs';
 import React from 'react';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 
 import { t } from '@/i18n/utils';
-import MyTabBar from '@/components/TabBar'
 
 export default function TabLayout() {
   return (
     <Tabs
-      tabBar={(props) => <MyTabBar {...props} />}
       initialRouteName="grand-prix"
       screenOptions={{
-        headerShown: false,
+        // headerShown: false,
       }}
     >
       <Tabs.Screen
         name="grand-prix"
         options={{
           title: t('GrandPrix', 'tabs'),
-          tabBarIcon: ({ color }) => <IconSymbol size={26} name="flag.pattern.checkered" color={color} />,
+          tabBarIcon: () => ({ sfSymbol: 'flag.checkered' }),
         }}
       />
       <Tabs.Screen
         name="standings"
         options={{
           title: t('Standings', 'tabs'),
-          tabBarIcon: ({ color }) => <IconSymbol size={26} name="trophy" color={color} />,
+          tabBarIcon: () => ({ sfSymbol: 'trophy' }),
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: t('settings', 'tabs'),
-          tabBarIcon: ({ color }) => <IconSymbol size={26} name="gear" color={color} />,
+          tabBarIcon: () => ({ sfSymbol: 'gear' }),
         }}
       />
-      <Tabs.Screen
+      {/* <Tabs.Screen
         name="index"
         options={{
           href: null,
         }}
-      />
+      /> */}
     </Tabs>
   );
 }
