430cc47970880eece6ff5cb315ee384f86d1c52c
Echo <819905193@qq.com>
2026-01-07 20:55:35 +0800

    fix: 修复获取比赛详情的返回值以匹配API结构

--- apis/f1api.ts
+++ apis/f1api.ts
@@ -4,5 +4,5 @@ const BASE = 'https://f1api.dev/api'
 
 export const getRaceDetails = async (season: string, round: string) => {
   const { data } = await axios.get(`${BASE}/${season}/${round}`)
-  return data.MRData.RaceTable.Races[0]
+  return data.race?.[0]
 }
